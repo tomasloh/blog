ac0f85c1cd8276273bb9e6256442dafdc7ea77ad
tomasloh_vm <tomasloh@gmail.com>
2011-06-14 00:20:12 +0400

    add ruby on rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ee9d4be861f32621b493afcd27cac22ab3f832102e44646980c1daf5f0c9a39724710016db71bd548e453319a93b7520695cfeb091a57bfb901f4424d10ac791'
